5d3263154525507bed343ba6329aa26733993bdd
rpyska <rafal.pyska@ergoproxy.pl>
2022-08-31 13:02:33 +0200

    Test UiCard in default layout

--- homework-front/layouts/default.vue
+++ homework-front/layouts/default.vue
@@ -1,10 +1,27 @@
 <template>
   <div>
+    <UiCard v-bind="sampleCard">
+      <template v-slot:content>
+        <p>{{ sampleCard.content }}</p>
+      </template>
+    </UiCard>
     <Nuxt />
   </div>
 </template>
 <script>
+  import UiCard from "homework-ui/src/components/Card/UiCard.vue";
   export default {
-
+    components: {
+      UiCard,
+    },
+    data() {
+      return {
+        sampleCard: {
+          title: 'Test title',
+          subtitle: 'Test subtitle',
+          content: 'Sample content',
+        }
+      }
+    },
   }
 </script>
