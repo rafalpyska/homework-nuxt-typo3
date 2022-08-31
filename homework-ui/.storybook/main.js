6dec72bf33ecd9a7c4e0368f5b784890ecdd925f
rpyska <rafal.pyska@ergoproxy.pl>
2022-08-31 13:02:42 +0200

    Remove sample stories

--- homework-ui/.storybook/main.js
+++ homework-ui/.storybook/main.js
@@ -1,12 +1,28 @@
+const path = require('path');
+
 module.exports = {
-  "stories": [
+  stories: [
     "../src/**/*.stories.mdx",
     "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
-  "addons": [
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
+    '@storybook/addon-postcss'
   ],
-  "framework": "@storybook/vue"
-}
+  framework: "@storybook/vue",
+  webpackFinal: async (config, { configType }) => {
+    // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
+    // You can change the configuration based on that.
+    // 'PRODUCTION' is used when building the static version of storybook.
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: ['style-loader', 'css-loader', 'sass-loader'],
+      include: path.resolve(__dirname, '../'),
+    });
+
+    // Return the altered config
+    return config;
+  },
+};
